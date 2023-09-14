b86a086e7b1336ada6d03e11ac487df0a8f2dcc6
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-09-14 10:22:06 -0300

    Trocando a lib

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const canvas = document.querySelector('#signatureCanvas')
-  const signaturePad = new SignaturePad(canvas)
+const canvas = document.querySelector('#signatureCanvas')
+const signaturePad = new SignaturePad(canvas)
 
-  var clearButton = document.getElementById('clearButton')
-  var saveButton = document.getElementById('saveButton')
-  var savedSignature = document.getElementById('savedSignature')
+var clearButton = document.getElementById('clearButton')
+var saveButton = document.getElementById('saveButton')
+var savedSignature = document.getElementById('savedSignature')
+clearButton.addEventListener('click', function () {
+  signaturePad.clear()
+})
 
-  clearButton.addEventListener('click', function () {
-    signaturePad.clear()
-  })
+saveButton.addEventListener('click', function () {
+  if (!signaturePad.isEmpty()) {
+    // Salve a assinatura como imagem base64
+    var signatureData = signaturePad.toDataURL()
 
-  saveButton.addEventListener('click', function () {
-    if (!signaturePad.isEmpty()) {
-      // Salve a assinatura como imagem base64
-      var signatureData = signaturePad.toDataURL()
-
-      // Exiba a imagem da assinatura salva
-      savedSignature.src = signatureData
-      savedSignature.style.display = 'block'
-    } else {
-      alert('Por favor, assine antes de salvar.')
-    }
-  })
+    // Exiba a imagem da assinatura salva
+    savedSignature.src = signatureData
+    savedSignature.style.display = 'block'
+  } else {
+    alert('Por favor, assine antes de salvar.')
+  }
 })
